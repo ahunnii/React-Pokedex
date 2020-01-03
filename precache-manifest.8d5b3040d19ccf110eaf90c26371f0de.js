c89de42b0e978e51ee3c171d792c2fc5df87a938
Andrew Hunn <ahunn@umich.edu>
2020-01-02 19:29:47 -0500

    Updates

---
+++ precache-manifest.8d5b3040d19ccf110eaf90c26371f0de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "018c259e70f6b951096d5af9dd51627d",
+    "url": "/React-Pokedex/index.html"
+  },
+  {
+    "revision": "4bca3b01b7adfddf3253",
+    "url": "/React-Pokedex/static/css/main.5499d147.chunk.css"
+  },
+  {
+    "revision": "d56859589f92d8a74581",
+    "url": "/React-Pokedex/static/js/2.48391acf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-Pokedex/static/js/2.48391acf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4bca3b01b7adfddf3253",
+    "url": "/React-Pokedex/static/js/main.f1dfbc04.chunk.js"
+  },
+  {
+    "revision": "588dead76a99d75616fa",
+    "url": "/React-Pokedex/static/js/runtime-main.e29a91e7.js"
+  }
+]);
